16604cdb82dbd5dd1571eb44ee0f528400f5e512
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-04-18 17:11:10 -0400

    minor formatting changes

--- word.js
+++ word.js
@@ -9,32 +9,32 @@ var Letter = require("./letter")
 var letter = new Letter();
 
 // Entire alphabet(?)
-const a = new Letter ("a", false);
-const b = new Letter ("b", false);
-const c = new Letter ("c", false);
-const d = new Letter ("d", false);
-const e = new Letter ("e", false);
-const f = new Letter ("f", false);
-const g = new Letter ("g", false);
-const h = new Letter ("h", false);
-const i = new Letter ("i", false);
-const j = new Letter ("j", false);
-const k = new Letter ("k", false);
-const l = new Letter ("l", false);
-const m = new Letter ("m", false);
-const n = new Letter ("n", false);
-const o = new Letter ("o", false);
-const p = new Letter ("p", false);
-const q = new Letter ("q", false);
-const r = new Letter ("r", false);
-const s = new Letter ("s", false);
-const t = new Letter ("t", false);
-const u = new Letter ("u", false);
-const v = new Letter ("v", false);
-const w = new Letter ("w", false);
-const x = new Letter ("x", false);
-const y = new Letter ("y", false);
-const z = new Letter ("z", false);
+const a = new Letter("a", false);
+const b = new Letter("b", false);
+const c = new Letter("c", false);
+const d = new Letter("d", false);
+const e = new Letter("e", false);
+const f = new Letter("f", false);
+const g = new Letter("g", false);
+const h = new Letter("h", false);
+const i = new Letter("i", false);
+const j = new Letter("j", false);
+const k = new Letter("k", false);
+const l = new Letter("l", false);
+const m = new Letter("m", false);
+const n = new Letter("n", false);
+const o = new Letter("o", false);
+const p = new Letter("p", false);
+const q = new Letter("q", false);
+const r = new Letter("r", false);
+const s = new Letter("s", false);
+const t = new Letter("t", false);
+const u = new Letter("u", false);
+const v = new Letter("v", false);
+const w = new Letter("w", false);
+const x = new Letter("x", false);
+const y = new Letter("y", false);
+const z = new Letter("z", false);
 
 // Word Constructor
 class Word {
@@ -48,9 +48,9 @@ class Word {
         var wordString = [];
 
         // Push letters from word to array
-        this.letterArray.forEach(function(letter) {
+        this.letterArray.forEach(function (letter) {
             wordString.push(letter.character);
-        }); 
+        });
 
         // Join letters
         console.log(wordString.join(""));
@@ -58,18 +58,17 @@ class Word {
     }
     guessCharacter(characterGuessed) {
 
-        // For every letter in the array check if it's been guessed 
-        this.letterArray.forEach(function(letter) {
+        this.letterArray.forEach(function (letter) {
 
-            // Check the letter against the guessed letter
+            // For every letter in the array check against the guessed letter
             console.log(letter.checkCharacter(characterGuessed));
-        }); 
+        });
     }
 }
 
 
-const butts = new Word ([b, u, t, t, s]);
+const butts = new Word([b, u, t, t, s]);
 
-// butts.toString();
+butts.toString();
 
 butts.guessCharacter("t");
