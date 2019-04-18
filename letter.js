d1f44527dba50a68f014410e350172b5a0b5002d
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-04-18 15:55:13 -0400

    added an alphabet which may or may not be the move

--- letter.js
+++ letter.js
@@ -23,5 +23,31 @@ class Letter {
     }
 }
 
-// const j = new Letter ("j", false);
-// console.log(j);
+const a = new Letter ("a", false);
+const b = new Letter ("b", false);
+const c = new Letter ("c", false);
+const d = new Letter ("d", false);
+const e = new Letter ("e", false);
+const f = new Letter ("f", false);
+const g = new Letter ("g", false);
+const h = new Letter ("h", false);
+const i = new Letter ("i", false);
+const j = new Letter ("j", false);
+const k = new Letter ("k", false);
+const l = new Letter ("l", false);
+const m = new Letter ("m", false);
+const n = new Letter ("n", false);
+const o = new Letter ("o", false);
+const p = new Letter ("p", false);
+const q = new Letter ("q", false);
+const r = new Letter ("r", false);
+const s = new Letter ("s", false);
+const t = new Letter ("t", false);
+const u = new Letter ("u", false);
+const v = new Letter ("v", false);
+const w = new Letter ("w", false);
+const x = new Letter ("x", false);
+const y = new Letter ("y", false);
+const z = new Letter ("z", false);
+
+module.exports = Letter;
--- word.js
+++ word.js
@@ -1,20 +1,58 @@
-var Letter = require("./letter");
-
 // An array of new Letter objects representing the letters of the underlying word
 // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
 // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
 
+// Require in letter to have all our properties available
+var Letter = require("./letter")
+
+// Create a new letter object 
+var letter = new Letter();
+
+// Entire alphabet(?)
+const a = new Letter ("a", false);
+const b = new Letter ("b", false);
+const c = new Letter ("c", false);
+const d = new Letter ("d", false);
+const e = new Letter ("e", false);
+const f = new Letter ("f", false);
+const g = new Letter ("g", false);
+const h = new Letter ("h", false);
+const i = new Letter ("i", false);
+const j = new Letter ("j", false);
+const k = new Letter ("k", false);
+const l = new Letter ("l", false);
+const m = new Letter ("m", false);
+const n = new Letter ("n", false);
+const o = new Letter ("o", false);
+const p = new Letter ("p", false);
+const q = new Letter ("q", false);
+const r = new Letter ("r", false);
+const s = new Letter ("s", false);
+const t = new Letter ("t", false);
+const u = new Letter ("u", false);
+const v = new Letter ("v", false);
+const w = new Letter ("w", false);
+const x = new Letter ("x", false);
+const y = new Letter ("y", false);
+const z = new Letter ("z", false);
+
+// Word Cobstructor
 class Word {
+
     constructor(letterArray) {
         this.letterArray = letterArray;
     }
-    returnString() {
-        letterArray.forEach(function(letter) {
+    toString() {
+        this.letterArray.forEach(function(letter) {
             letter.returnCharacter();
-        }).join(" ");  
+        }).join("");  
     }
     guessCharacter(character) {
         character.checkCharacter();
     }
 }
 
+
+const butts = new Word ([b, u, t, t, s]);
+
+console.log(butts);
